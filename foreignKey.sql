6937ffa96e0ad04366c255914de03a669bf08336
jahid <jahidmorol2@gmail.com>
2024-03-15 20:01:35 +0600

    explore join

--- foreignKey.sql
+++ foreignKey.sql
@@ -7,14 +7,12 @@ CREATE Table "user"(
 CREATE Table post(
     id SERIAL PRIMARY KEY,
     title text NOT NULL,
-    user_id INTEGER REFERENCES "user"(id) on delete CASCADE 
-    -- user_id INTEGER REFERENCES "user"(id) on delete set DEFAULT DEFAULT 2 
+    user_id INTEGER REFERENCES "user"(id)
 )
 
-ALTER Table post
-    alter COLUMN user_id set NOT null;
 
-    INSERT INTO "user" (username) VALUES
+
+INSERT INTO "user" (username) VALUES
 ('akash'),
 ('batash'),
 ('sagor'),
@@ -26,25 +24,15 @@ INSERT INTO post (title, user_id) VALUES
 ('Exploring adventures with Sagor.🌟', 4),
 ('Nodi''s wisdom always leaves me inspired. 📚', 4);
 
+
 DROP Table post;
 DROP Table "user";
 
 SELECT * from "user";
 SELECT * from post;
 
+SELECT title, username FROM post
+JOIN "user" ON post.user_id = "user".id
 
--- Insertion constraint on INSERT post
--- Attempting to insert a post with a user ID that does not exist
--- Inserting a post with a valid user ID
--- Attempting to insert a post without specifying a user ID
-
-
-DELETE FROM "user" 
-    WHERE id = 4;
-
-
--- Deletion constraint on DELETE user
--- Restrict Deletion -> ON DELETE RESTRICT / ON DELETE NO ACTION (default)
--- Cascading Deletion -> ON DELETE CASCADE
--- Setting NULL -> ON DELETE SET NULL
--- Set Default value -> ON DELETE SET DEFAULT
+SELECT * FROM post as p
+JOIN "user" u on p.user_id = u.id;
